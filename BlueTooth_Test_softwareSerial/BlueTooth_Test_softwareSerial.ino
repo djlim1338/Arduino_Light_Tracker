7434fdabb766d5b7a588b113c3a0dbdec5c50bee
djlim1338 <127079174+djlim1338@users.noreply.github.com>
2024-02-13 14:46:47 +0900

    Add files via upload

---
+++ BlueTooth_Test_softwareSerial/BlueTooth_Test_softwareSerial.ino
@@ -0,0 +1,32 @@
+// BlueTooth_Test_softwareSerial
+
+#include <SoftwareSerial.h>
+
+#define RXPin 2
+#define TXPin 3
+
+SoftwareSerial BlueToothSerial(RXPin, TXPin);
+
+char Serial_data, BlueToothSerial_data;
+void setup()
+{
+  Serial.begin(9600);
+  BlueToothSerial.begin(9600);
+}
+
+void loop()
+{
+  if(Serial.available())
+  {
+    Serial_data = Serial.read();
+    Serial.write(Serial_data);
+    BlueToothSerial.write(Serial_data);
+  }
+  if(BlueToothSerial.available())
+  {
+    BlueToothSerial_data = BlueToothSerial.read();
+    Serial.write(BlueToothSerial_data);
+  }
+}
+
+
